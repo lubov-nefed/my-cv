938d0185c75192f2603f9991ebcdb420394536ab
lubov-nefed <love.nefed@gmail.com>
2024-01-18 14:50:30 +0300

    feat: add language switch

---
+++ js/lang-dependent-content.js
@@ -0,0 +1,48 @@
+const dataLang = {
+  ru: {
+    'page-title': 'Любовь Нефёдова Junior Frontend Developer',
+    'burger-heading': 'Любовь Нефёдова',
+    'burger-nav-item': ['Обо мне', 'Образование и Навыки', 'Проекты', 'Код', 'Контакты'],
+    'header-nav-item': ['Обо мне', 'Образование и Навыки', 'Проекты', 'Код', 'Контакты'],
+    'lang-btn-span': ['анг', 'ру'],
+    'first-section-heading': ['Привет, я ', 'Любовь Нефёдова'],
+    'section-paragraph': [
+      'Я стремлюсь стать квалифицированным фронтенд разработчиком с сильными техническими навыками и отличными навыками командной работы.',
+      `Я младший фронтенд разработчик, нацеленый на долгосрочное обучение и приобретение опыта работы над реальными проектами. 
+      Я открыта для новых интересных и значимых проектов. Эффективная коммуникация и гибкость это то, что лучше всего помогает мне в командной работе.`,
+    ],
+    'first-section-btn': 'Связаться',
+    'section-heading': ['Обо мне', 'Образование и навыки', 'Курсы', 'Навыки', 'Проекты', 'Пример кода', 'Языки', 'Контакты'],
+    'education-item': ['Университет Лобачевского, бакалавр психологии <br> 2014 - 2018', 'Университет Лобачевского, Физкультура и спорт (профессиональная переподготовка)<br>Март 2019 - Июнь 2019'],
+    'courses-item': ['Rs School, Frontend Stage #0 <br> Май 2022 – Сентябрь 2022', 'FreeCodeCamp <br> Январь 2021 - Сентябрь 2022 <br> 251 балл', 'Mimo App <br> Ноябрь 2021 - Май 2022 <br> 84% Веб Разработка'],
+    'skills-item': ['React  (основы)', 'BEM (классы и структура файлов)'],
+    'project-name': ['Компьютерный курс', 'Сафари', 'Экспофорум'],
+    'lang-item': ['Русский (родной)', 'Английский (B1-B2)'],
+    'contacts-item': ['эл.адрес: love.nefed@gmail.com', 'Местонахождение: Россия, Нижний Новгород'],
+    'footer-copyright': '© 2022 Любовь Нефёдова',
+  },
+  en: {
+    'page-title': 'Lyubov Nefyodova Junior Frontend Developer',
+    'burger-heading': 'Lyubov Nefyodova',
+    'burger-nav-item': ['About', 'Education & Skills', 'Projects', 'Code', 'Contacts'],
+    'header-nav-item': ['About', 'Education & Skills', 'Projects', 'Code', 'Contacts'],
+    'lang-btn-span': ['en', 'ru'],
+    'first-section-heading': ['Hi, I’m ', 'Lyubov Nefyodova'],
+    'section-paragraph': [
+      'I aspire to become a qualified frontend developer with strong technical skills and good performance as a team member.',
+      `I’m junior front-end developer interested in continuous learning and gaining real project experience. 
+      I’m opened to new interesting and significant projects. My best soft skills are effective communication and flexibility in teamwork.`,
+    ],
+    'first-section-btn': 'Contact me',
+    'section-heading': ['About me', 'Education & Skills', 'Courses', 'Skills', 'Projects', 'Code sample', 'Languages', 'Contacts'],
+    'education-item': ['Lobachevsky University, B.A. in Psychology <br> 2014 - 2018', 'Lobachevsky University, Physical Culture and Sports (short-program) <br> March 2019 - June 2019'],
+    'courses-item': ['Rs School, Frontend Stage #0 <br> May 2022 – September 2022', 'FreeCodeCamp <br> January 2021 - September 2022 <br> 251 total points', ' Mimo App <br> November 2021 - May 2022 <br> 84% Web Development'],
+    'skills-item': ['React (basics)', 'BEM (naming and file structure)'],
+    'project-name': ['Computer Course', 'Safari', 'Expoforum'],
+    'lang-item': ['Russian (native)', 'English (B1-B2)'],
+    'contacts-item': ['e-mail: love.nefed@gmail.com', 'Location: Russia, Nizhny Novgorod'],
+    'footer-copyright': '© 2022 Lyubov Nefyodova',
+  },
+};
+
+export default dataLang;
--- js/script.js
+++ js/script.js
@@ -1,12 +1,5 @@
-/* Theme switcher */
-const switchTheme = () => {
-  const themeStyleSheet = document.querySelector('#theme-styles');
-  if (themeStyleSheet.getAttribute('href') === 'css/themes/light-theme.css') themeStyleSheet.setAttribute('href', 'css/themes/dark-theme.css');
-  else themeStyleSheet.setAttribute('href', 'css/themes/light-theme.css');
-};
-const themeBtn = document.querySelector('.theme-switcher');
-themeBtn.addEventListener('click', switchTheme);
-/* ----------- */
+// eslint-disable-next-line import/extensions
+import dataLang from './lang-dependent-content.js';
 
 /* Burger Menu */
 const burgerNav = document.querySelector('.burger__nav');
@@ -30,3 +23,41 @@ const burgerCloseBtn = document.querySelector('.burger__close-btn');
 burgerCloseBtn.addEventListener('click', closeBurgerMenu);
 const burgerAnchors = document.querySelectorAll('.burger__anchor');
 burgerAnchors.forEach((anchor) => anchor.addEventListener('click', closeBurgerMenu));
+
+/* Theme switcher */
+const switchTheme = () => {
+  const themeStyleSheet = document.querySelector('#theme-styles');
+  if (themeStyleSheet.getAttribute('href') === 'css/themes/light-theme.css') themeStyleSheet.setAttribute('href', 'css/themes/dark-theme.css');
+  else themeStyleSheet.setAttribute('href', 'css/themes/light-theme.css');
+};
+const themeBtn = document.querySelector('.theme-switcher');
+themeBtn.addEventListener('click', switchTheme);
+
+/* Lang switcher */
+let currentLang = 'en';
+const langSwitcherHandler = () => {
+  const langSwitcherAnimation = () => {
+    const langSwitcherCircle = document.querySelector('.lang-switcher__circle');
+    if (window.getComputedStyle(langSwitcherCircle).left === '7px') langSwitcherCircle.style.left = '28px';
+    else langSwitcherCircle.style.left = '7px';
+  };
+  langSwitcherAnimation();
+
+  const switchLang = () => {
+    if (currentLang === 'en') currentLang = 'ru';
+    else currentLang = 'en';
+    Object.keys(dataLang[currentLang]).forEach((key) => {
+      if (typeof dataLang[currentLang][key] === 'object') {
+        const collection = document.querySelectorAll(`[data-lang=${key}]`);
+        for (let i = 0; i < collection.length; i++) {
+          const element = collection[i];
+          element.innerHTML = dataLang[currentLang][key][i];
+        }
+      } else document.querySelector(`[data-lang=${key}]`).innerHTML = dataLang[currentLang][key];
+    });
+  };
+  switchLang();
+};
+
+const langSwitcher = document.querySelector('.lang-switcher');
+langSwitcher.addEventListener('click', langSwitcherHandler);
